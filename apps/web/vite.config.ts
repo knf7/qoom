d68ff5385bc40feb2a75715e8bcf0c307f98df76
Kaled Al shmmre <kaledalshmmre@MacBook-Air-Kaled.local>
2026-06-25 16:57:04 +0300

    fix(deploy): force vite to output to root dist to fix vercel 404

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: process.env.VERCEL ? '../../dist' : 'dist',
+    outDir: '../../dist',
     emptyOutDir: true,
   },
   server: {
